cebd68554eff602060ffdf364523f996c65c5719
ezmezdev <ezmezdev@gmail.com>
2026-04-23 23:55:15 -0300

    fd

--- js/config.example.js
+++ js/config.example.js
@@ -6,6 +6,6 @@
 // 3. config.js está en .gitignore y NO se sube a GitHub
 // ============================================================
 
-const SUPABASE_URL = "TU_URL_DE_SUPABASE_AQUI";
-const SUPABASE_ANON_KEY = "TU_ANON_KEY_AQUI";
+const SUPABASE_URL = "https://khnydtpsavqrpncgjfbc.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtobnlkdHBzYXZxcnBuY2dqZmJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NzA1MTUsImV4cCI6MjA5MjU0NjUxNX0.0S04QKacoKSL9ieA826jN_vh4bnAInPMgx79-gez64A";
 const MP_PUBLIC_KEY = "TU_PUBLIC_KEY_DE_MERCADOPAGO_AQUI";
